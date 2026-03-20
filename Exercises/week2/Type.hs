34e53da2340d45f534bbb607970adf58056aa1f8
Ayooluwa Fadeyibi <fadeyibi2016@gmail.com>
2026-03-20 23:21:14 +1000

    Completed some of the type questions

--- Exercises/week2/Type.hs
+++ Exercises/week2/Type.hs
@@ -5,13 +5,13 @@ typeA (f, a) = f a
 typeB f g = (g, [head (f (g, g))])
 
 --typeC :: (a -> b -> c -> d -> r) -> ((a, b) -> (c, d) -> r)
-typeC f = undefined
+typeC f a b = f (fst a) (snd a) (fst b) (snd b)
 
 --typeD :: (c -> a -> b) -> (c -> a) -> (c -> b)
-typeD f g = undefined
+typeD f g c = f c (g c)
 
 --typeE :: ((c -> a) -> b) -> (c -> a) -> (c -> b)
-typeE = undefined
+typeE f g = f (\a -> g a)
 
 --typeF :: (a -> b) -> ((a -> c) -> d) -> ((b -> c) -> [d])
-typeF = undefined
+typeF f g = undefined 
