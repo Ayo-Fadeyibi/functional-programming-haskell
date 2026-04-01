86f50ecaaec4f0910561ec3af9b62413452374bf
Ayooluwa Fadeyibi <fadeyibi2016@gmail.com>
2026-04-01 10:24:55 +1000

    Given an age in seconds, calculate how old someone would be on other planets

---
+++ Exercises/week2/SpaceAge.hs
@@ -0,0 +1,20 @@
+module SpaceAge (Planet(..), ageOn) where
+
+data Planet = Mercury
+            | Venus
+            | Earth
+            | Mars
+            | Jupiter
+            | Saturn
+            | Uranus
+            | Neptune
+
+ageOn :: Planet -> Float -> Float
+ageOn Mercury seconds = seconds / 7600543.82
+ageOn Venus seconds = seconds / 19414149.05
+ageOn Earth seconds = seconds / 31557600
+ageOn Mars seconds = seconds / 59354032.69
+ageOn Jupiter seconds = seconds / 374355659.1
+ageOn Saturn seconds = seconds / 929292362.9
+ageOn Uranus seconds = seconds / 2651370020
+ageOn Neptune seconds = seconds / 5200418560
